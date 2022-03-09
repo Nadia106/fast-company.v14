6e7afab8fe7fbf883c23cf9eda3c6a79d42c1677
Nadia106 <95348769+Nadia106@users.noreply.github.com>
2022-03-10 02:40:06 +0500

    Add files via upload

---
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Quality from "./quality";
+import { useQualities } from "../../../hooks/useQualities";
+
+const QualitiesList = ({ qualities }) => {
+    const { getQuality } = useQualities();
+    const qualy = getQuality(qualities);
+    return (
+        <>
+            {qualy.map((qual) => (
+                <Quality key={qual._id} {...qual} />
+            ))}
+        </>
+    );
+};
+
+QualitiesList.propTypes = {
+    qualities: PropTypes.array
+};
+
+export default QualitiesList;
---
+++ src/app/hooks/useQualities.jsx
@@ -0,0 +1,62 @@
+import React, { useContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import QualityService from "../services/qualities.service";
+import { toast } from "react-toastify";
+
+const QualityContext = React.createContext();
+
+export const useQualities = () => {
+    return useContext(QualityContext);
+};
+
+export const QualityProvider = ({ children }) => {
+    const [isLoading, setLoading] = useState(true);
+    const [qualities, setQualities] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+
+    useEffect(() => {
+        getQualitiesList();
+    }, []);
+
+    async function getQualitiesList() {
+        try {
+            const { content } = await QualityService.get();
+            setQualities(content);
+            setLoading(false);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+
+    function getQuality(id) {
+        const qualy = id.map((r) => {
+            return qualities.find((q) => q._id === r);
+        });
+        return qualy;
+    }
+
+    return (
+        <QualityContext.Provider value={{ isLoading, qualities, getQuality }}>
+            {children}
+        </QualityContext.Provider>
+    );
+};
+
+QualityProvider.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
